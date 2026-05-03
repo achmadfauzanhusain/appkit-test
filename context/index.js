65010f29b0bfac19da9602fda6891f3b0d188b73
achmadfauzanhusain <achmadfauzan1340@gmail.com>
2026-05-03 21:20:11 +0700

    setup context

---
+++ context/index.js
@@ -0,0 +1,32 @@
+import { createAppKit } from "@reown/appkit/react";
+import { EthersAdapter } from "@reown/appkit-adapter-ethers";
+import { mainnet, arbitrum } from "@reown/appkit/networks";
+
+const projectId = process.env.NEXT_PUBLIC_PROJECT_ID
+
+if(!projectId) {
+  throw new Error("project id is not defined");
+}
+
+const metadata = {
+  name: "Appkit-test",
+  description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod, nisl eget consectetur sagittis, nisl nunc.",
+  url: "http://localhost:3000",
+  icons: ["./vercel.svg"],
+};
+
+createAppKit({
+  adapters: [new EthersAdapter()],
+  metadata,
+  networks: [mainnet, arbitrum],
+  projectId,
+  features: {
+    analytics: true,
+  },
+});
+
+export function AppKit() {
+  return (
+    <YourApp />
+  );
+}
